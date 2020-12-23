ab539dcd0a522e6df9e107eb222650535e2a2199
Denis Edrey <denis.alves.hu@gmail.com>
2020-12-23 17:35:25 -0300

    implemented insert into SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -32,6 +33,14 @@ public class Program {
 		list = sellerDao.findAll();
 		
 		list.forEach(x -> System.out.println(x));
+		
+		System.out.println("\n--- Test 04: seller Insert ---");
+		
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		
+		sellerDao.insert(newSeller);
+		
+		System.out.println("Inserted, new Id = " + newSeller.getId());
 		
 	}
 
