a27402c214a0d4ec3799cd4f5f9ecff155204eae
Denis Edrey <denis.alves.hu@gmail.com>
2020-12-23 16:21:24 -0300

    created DaoFactory and implementation of SellerDao

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -16,6 +18,8 @@ public class Program {
 		Seller sel = new Seller(1, "john", "john@gmail.com", new Date(), 3000.0, dep);
 		
 		System.out.println(sel);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 	}
 
