882d8b664d4009cc378ecb3deb49b2c08de07416
Denis Edrey <denis.alves.hu@gmail.com>
2020-12-23 16:55:17 -0300

    implemented FindById on SellerDaoJDBC, also fixed some timezone shennanigans on db.properties

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,25 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department dep = new Department(1, "electronics");
-		
-		System.out.println(dep);
-		
-		Seller sel = new Seller(1, "john", "john@gmail.com", new Date(), 3000.0, dep);
-		
-		System.out.println(sel);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 
 	}
 
