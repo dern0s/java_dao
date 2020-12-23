6f4cad26401198c07d9cd339e9ebc9c94cbbd712
Denis Edrey <denis.alves.hu@gmail.com>
2020-12-23 16:07:11 -0300

    created Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,10 @@ public class Program {
 		Department dep = new Department(1, "electronics");
 		
 		System.out.println(dep);
+		
+		Seller sel = new Seller(1, "john", "john@gmail.com", new Date(), 3000.0, dep);
+		
+		System.out.println(sel);
 
 	}
 
