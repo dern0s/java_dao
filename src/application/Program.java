8f87b687572f74607bdde3c17f2e088d82382de3
Denis Edrey <denis.alves.hu@gmail.com>
2020-12-23 16:00:41 -0300

    added department

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department dep = new Department(1, "electronics");
+		
+		System.out.println(dep);
+
+	}
+
+}
